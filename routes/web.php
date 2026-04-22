b6ca5314941e7f3dd1b2d02913cdc366b2c42291
Jose Carlos Ibarra Herrera <jcibarrah1423@gmail.com>
2026-04-22 16:53:51 -0500

    feat: add base health check route to web routes

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return response()->json([
-        'service' => 'My API Name',
-        'status' => 'running',
-        'php_version' => PHP_VERSION,
-        'laravel_version' => app()->version()
+        'status' => 'running'
     ]);
 });
